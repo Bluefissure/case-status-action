2b64a4673a61f79172250da5f2f31ac21267b0dd
Bluefissure <bluefissure@foxmail.com>
2022-05-25 14:49:44 -0500

    fix input_receipt_number

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$RECEIPT_NUMBER" ]; then
+if [ -z "$INPUT_RECEIPT_NUMBER" ]; then
   echo '::error::Required Receipt Number 2'
   exit 1
 fi
 
 echo "Running case checker"
-python main.py --receipt-number "$RECEIPT_NUMBER"
+python main.py --receipt-number "$INPUT_RECEIPT_NUMBER"

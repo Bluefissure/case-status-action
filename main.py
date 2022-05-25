f84a64714a51c374c2dff915c38a8e4913cc5fd2
Bluefissure <bluefissure@foxmail.com>
2022-05-25 15:04:14 -0500

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
